f3444e08118d92c6d56396fa162c12f30f6f1951
Austin O <augustineug@gmail.com>
2022-09-11 18:30:04 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  * returns 0
  */
 int main(void){
-	puts("\"Programming is like building a multilingual puzzle"\n);
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle")\n;
+	return (0);
 }
