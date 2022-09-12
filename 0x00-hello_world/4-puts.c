08916ba9402a05ab4e38e3763173a7cd4cbbaca0
Austin O <augustineug@gmail.com>
2022-09-11 18:45:25 -0700

    printf function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  * returns 0
  */
 int main(void){
-	puts("\"Programming is like building a multilingual puzzle")\n;
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main entry point
+ * desc: printing a string using printf function
+ * return 0
+ */
+int main(void){
+	printf("%s","with proper grammar, but the outcome is a piece of art,\n");
+	return (0)
+}
