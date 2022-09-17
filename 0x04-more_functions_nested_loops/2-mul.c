3aba14a3e7603e807bbe402295067a4380e77729
Austin O <augustineug@gmail.com>
2022-09-16 18:02:19 -0700

    sum of two digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: is an integer
+ * @b: is another integer
+ *
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
+
