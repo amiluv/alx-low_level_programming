39005190ffc7ffc4722edba9ec63efb88275d2f9
Austin O <augustineug@gmail.com>
2022-09-19 15:05:28 -0700

    reseting the pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the value the pointer points to
+ * @n: is a pointer
+ *
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This project focuses on pointers, strings and arrays
