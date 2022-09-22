c9673156d1e8e1ae4333ace2acb07dd695a24ec2
Austin O <augustineug@gmail.com>
2022-09-22 04:01:14 -0700

    print a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - prints a[2] = 98
  * followed by a new line
@@ -24,5 +25,5 @@ int main(void)
 	p[5] = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
-return (0);
+	return (0);
 }
