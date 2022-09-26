56ab127c200396bacb9dac4e4b443678812404f3
Austin O <augustineug@gmail.com>
2022-09-26 01:51:58 -0700

    coppying memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - coppies memory area
+ * @dest: pointer to char parameters
+ * @src: pointer to char parameters
+ * @n: size
+ *
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
