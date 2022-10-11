c738fc37ed26e9f5c4f65e2b2422fbf1bc4f2345
Austin O <augustineug@gmail.com>
2022-10-11 05:07:34 -0700

    preprocessor codes

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - prints the name of the file program was compiled from
  *
  * Return: Always 0
@@ -10,6 +9,6 @@
 
 int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
