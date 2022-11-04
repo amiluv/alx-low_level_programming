6b4d170f093d91d8f6f319e63e9b709558e53b1e
Austin O <augustineug@gmail.com>
2022-11-03 20:59:51 -0700

    A program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -337,7 +337,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_abi(header->e_ident);
 	print_type(header->e_type, header->e_ident);
 	print_entry(header->e_entry, header->e_ident);
-	ree(header);
+	free(header);
 	close_elf(o);
 	return (0);
 }
